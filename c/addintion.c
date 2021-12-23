6d2a5300ec9f3794e4db6a7709f893269dae4760
roms <rusirusamaraweera749@gmail.com>
2021-12-24 04:30:07 +0530

    example web

---
+++ c/addintion.c
@@ -0,0 +1,14 @@
+#includ <stdio.h>
+
+int main(void)
+{
+	int x = get_int("x:");
+	int y = get_int ("Y");
+	printf("%i\n", answer is x+y);
+	
+	int r = get_int("r:");
+	float t =get_float("t:");
+	float z = (float)r/ t;
+	printf("%f\n", answer is z);
+	
+}
